26b9f9e62e71e2d8d2e25076cfc04fe0eceef08d
Arthitphk <128705950+Arthitphk@users.noreply.github.com>
2024-02-26 01:52:19 +0700

    Fix Text
    Fix Text-White in  dark-mode

--- FinalProject/src/Pages/Categories.tsx
+++ FinalProject/src/Pages/Categories.tsx
@@ -63,7 +63,7 @@ const Categories = () => {
         </div>
         <div className="dark:bg-gray-700 py-24 sm:py-32 mt-44">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+        <h2 className="text-center text-lg font-semibold leading-8 dark:text-white">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos explicabo eaque id earum temporibus veniam laborum similique obcaecati sunt reprehenderit dolores autem, facere commodi, magnam rerum atque voluptatibus perferendis soluta?
         </h2>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
