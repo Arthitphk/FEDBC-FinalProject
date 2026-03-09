d38a986d82f1ccc3ca92a0e73174d8a36966650d
Arthitphk <128705950+Arthitphk@users.noreply.github.com>
2026-03-09 14:48:58 +0700

    add icon navbar

--- FinalProject/src/Components/Navbar.tsx
+++ FinalProject/src/Components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Theme from "./Theme";
-
+import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
+import InfoIcon from '@mui/icons-material/Info';
+import RoomServiceIcon from '@mui/icons-material/RoomService';
 
 const Navbar = () => {
-  
-  const [open , setOpen] = useState(false)
+
+  const [open, setOpen] = useState(false)
 
   const toggleMenu = () => {
     setOpen(!open)
@@ -13,51 +15,53 @@ const Navbar = () => {
 
 
   return (
-    <nav className="p-4 dark:bg-gray-800 container max-w-7xl mx-auto">
-      <div className="flex  justify-between">
+    <nav className="shadow-md">
+      <div className="p-4 dark:bg-gray-800 container max-w-7xl mx-auto ">
+        <div className="flex  justify-between">
           <a href="/Home">
             <div className="text-black text-2xl font-bold dark:text-white ">Coding
-            <span className="text-teal-500">Tech</span>
-          </div>
+              <span className="text-teal-500">Tech</span>
+            </div>
           </a>
 
           <div className="md:hidden">
             <button id="menu-toggle" className="dark:text-white">
-                  <svg  
-                  fill="none" 
-                  stroke="currentColor" 
-                  strokeLinecap="round" 
-                  strokeLinejoin="round" 
-                  strokeWidth={2} 
-                  viewBox=" 0 0 24 24"
-                  className="w-6 h-6"
-                  onClick={toggleMenu}
-                  >
-                    <path d="M4 6H16M4 12h16M4 18h16"></path>
-                  </svg>
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                viewBox=" 0 0 24 24"
+                className="w-6 h-6"
+                onClick={toggleMenu}
+              >
+                <path d="M4 6H16M4 12h16M4 18h16"></path>
+              </svg>
             </button>
           </div>
 
 
           <ul className="hidden md:flex space-x-4 p-1">
-              {/* <li><a href="/Home" className="dark:text-white font-bold dark:hover:text-teal-500">Home</a></li> */}
+            {/* <li><a href="/Home" className="dark:text-white font-bold dark:hover:text-teal-500">Home</a></li> */}
 
-              <li><a href="#" className="dark:text-white font-bold dark:hover:text-teal-500  hover:text-purple-700">หลักสูตรทั้งหมด</a></li>
-              <li><a href="#" className="dark:text-white font-bold dark:hover:text-teal-500">เกี่ยวกับเรา</a></li>
-              <li><a href="#" className="dark:text-white font-bold dark:hover:text-teal-500">บริการ</a></li>
+            <li><a href="#" className="dark:text-white font-bold dark:hover:text-teal-500  hover:text-purple-700"><CollectionsBookmarkIcon className="p-1"/>หลักสูตรทั้งหมด</a></li>
+            <li><a href="#" className="dark:text-white font-bold dark:hover:text-teal-500"><InfoIcon className="p-1"/>เกี่ยวกับเรา</a></li>
+            <li><a href="#" className="dark:text-white font-bold dark:hover:text-teal-500">< RoomServiceIcon className="p-1"/>บริการ</a></li>
           </ul>
           <div className="hidden md:flex">
-            <Theme/>
+            <Theme />
           </div>
+        </div>
       </div>
       {open ? (
-            <ul className="flex-col md:hidden">
-              <li className="py-3"><a href="#" className="dark:text-white font-bold dark:hover:text-teal-500">Home</a></li>
-              <li className="py-3"><a href="#" className="dark:text-white font-bold dark:hover:text-teal-500">About</a></li>
-              <li className="py-3"><a href="#" className="dark:text-white font-bold dark:hover:text-teal-500">Service</a></li>
-              <Theme/>
-          </ul>
-          ) : null}
+        <ul className="flex-col md:hidden pb-5 pl-3">
+            <li className="p-2"><a href="#" className="dark:text-white font-bold dark:hover:text-teal-500  hover:text-purple-700"><CollectionsBookmarkIcon className="p-1"/>หลักสูตรทั้งหมด</a></li>
+            <li className="p-2"><a href="#" className="dark:text-white font-bold dark:hover:text-teal-500"><InfoIcon className="p-1"/>เกี่ยวกับเรา</a></li>
+            <li className="p-2"><a href="#" className="dark:text-white font-bold dark:hover:text-teal-500">< RoomServiceIcon className="p-1"/>บริการ</a></li>
+          <Theme />
+        </ul>
+      ) : null}
     </nav>
   )
 }
